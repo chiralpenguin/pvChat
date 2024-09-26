768566b5f7a9310136a8f0a0ad37dd31598f876c
chiralpenguin <chiralpenguin@users.noreply.github.com>
2024-09-26 19:42:58 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# pvChat
+This plugin is designed to replace chat formatting and displayname from ChatManager as well as features from other plugins used regularly on [Purity Vanilla](https://www.purityvanilla.com/).
+
+This plugin depends on [pvCore]("https://github.com/chiralpenguin/pvCore") for data storage and other centralised features.
---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "com.purityvanilla"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("dev.folia", "folia-api", "1.20.1-R0.1-SNAPSHOT")
+    compileOnly("com.purityvanilla", "pvLib", "1.0")
+
+    implementation("org.mariadb.jdbc", "mariadb-java-client", "3.4.1")
+    implementation("com.zaxxer", "HikariCP", "5.1.0")
+}
+
+tasks.shadowJar {
+    dependsOn(tasks.build)
+    archiveClassifier.set("") // This removes the default "-all" classifier
+    archiveFileName.set("pvChat.jar")
+}
+
+val testServerPluginsPath: String by project
+tasks {
+    val copyToServer by creating(Copy::class) {
+        dependsOn("shadowJar")
+        from(layout.buildDirectory.file("libs"))
+        include("pvChat.jar") // Change to "plugin-version.jar" if no shadowing
+        into(file(testServerPluginsPath)) // Use the externalized path here
+    }
+}
