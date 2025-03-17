793b85f719482738dd9ddf34ec0ae648577423c0
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2025-03-17 01:56:39 +0000

    Custom ChatRenderer from messages.json

--- src/main/java/com/purityvanilla/pvchat/listeners/AsyncChatListener.java
+++ src/main/java/com/purityvanilla/pvchat/listeners/AsyncChatListener.java
@@ -3,8 +3,12 @@ package com.purityvanilla.pvchat.listeners;
 import com.purityvanilla.pvchat.PVChat;
 import com.purityvanilla.pvcore.PVCore;
 import com.purityvanilla.pvcore.util.FormatCodeParser;
+import io.papermc.paper.chat.ChatRenderer;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -21,11 +25,21 @@ public class AsyncChatListener implements Listener {
     public void onAsyncChat(AsyncChatEvent event) {
         Player player = event.getPlayer();
 
-        // Custom ChatRenderer defined in config
-
         // Replace permitted format codes with proper Component formatting
         String rawMessage = PlainTextComponentSerializer.plainText().serialize(event.message());
         Component formattedMessage = PVCore.getAPI().parsePlayerFormatString(rawMessage, player, FormatCodeParser.Context.CHAT);
-        event.message(formattedMessage);
+        Component prefix = PVCore.getAPI().getPlayerAPI().getPlayerPrefix(player);
+        Component suffix = PVCore.getAPI().getPlayerAPI().getPlayerSuffix(player);
+
+        // Custom ChatRenderer defined as message
+        event.renderer((source, sourceDisplayName, message, viewer) ->
+                plugin.config().getMessage("chat-renderer",
+                TagResolver.resolver(
+                        Placeholder.component("prefix", prefix),
+                        Placeholder.component("displayname", player.displayName()),
+                        Placeholder.component("suffix", suffix),
+                        Placeholder.component("message", formattedMessage)
+                )
+        ));
     }
 }
